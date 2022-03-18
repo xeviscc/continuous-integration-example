88593b166d278d2658c02368ca862f0501e2e868
Xavier Cases Camats <xeviscc@gmail.com>
2022-03-18 09:31:10 +0100

    test change 200 per 500

--- app/test.py
+++ app/test.py
@@ -15,7 +15,7 @@ class TestDockerapp(unittest.TestCase):
     def test_load_value(self):
         self.app.post('/', data=dict(submit='save', key='2', cache_value='two'))
         response = self.app.post('/', data=dict(submit='load', key='2'))
-        assert response.status_code == 200 
+        assert response.status_code == 500 
         assert b'2' in response.data
         assert b'two' in response.data
 
